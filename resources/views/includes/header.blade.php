f2e4849f3d6bb79793f72b86fbf24fa59bc75f8f
Aleandro2119 <aleandro.basilico@gmail.com>
2022-03-30 00:52:06 +0200

    Add Header

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,19 @@
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="navbar-brand" href="#">Navbar</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Movie</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
